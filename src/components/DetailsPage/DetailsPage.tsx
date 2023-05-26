e11f098c662834ec177d1e64ee18af63c9b2275c
Alena Shapovalova <avshapovalova31@gmail.com>
2023-05-26 15:33:18 +0200

    feat: add styles to DeatailPage

--- src/components/DetailsPage/DetailsPage.tsx
+++ src/components/DetailsPage/DetailsPage.tsx
@@ -1,10 +1,9 @@
-import axios from 'axios';
-import { useState, useEffect } from 'react';
 import { Card, Container, ListGroup } from 'react-bootstrap';
-import { Link, useLoaderData, useLocation, useParams } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { Post } from '../../models/Post';
 import Comments from '../MainPage/Comments';
 import { User } from '../../models/User';
+import "./DetailsPage.css";
 
 const DetailsPage = () => {
     const { state } = useLocation();
@@ -14,27 +13,32 @@ const DetailsPage = () => {
     return (
         <Container>
             <Card className='rounded-4 bg-white m-2'>
-                <Card.Header><Link to=".." relative="path" className="btn btn-primary">Назад</Link></Card.Header>
-                <Card.Body className="p-10 text-center">
-                        <img src="imgs/woman.png" alt="avatar" />
-                    <Card.Title>{ user.name }</Card.Title>
+                <Card.Header><Link to=".." relative="path" className="btn btn-dark">Назад</Link></Card.Header>
+                <Card.Img className="banner" variant="top" src="../../../imgs/Banner.png" />
+                <Card.Body className="p-10">
+                    <div className="d-flex-column text-center">
+                        <img className="user-avatar" src="imgs/woman-big.png" alt="avatar" />
+                        <Card.Title>{user.name}</Card.Title>
+                    </div>
+
+                    <ListGroup className='d-flex w-100'>
+                        {posts.map((post) => (
+                            <Card key={post.id} className='rounded-4 bg-white m-2'>
+                                <Card.Body className='p-10'>
+                                    <div className="d-flex align-items-center">
+                                        <img src="imgs/woman.png" alt="avatar" />
+                                        <Card.Subtitle className="m-2 text-muted">{user.name}</Card.Subtitle>
+                                    </div>
+                                    <Card.Title>{post.title}</Card.Title>
+                                    <Card.Text>{post.body}</Card.Text>
+                                    <Comments postId={post.id} />
+                                </Card.Body>
+                            </Card>
+                        ))}
+                    </ListGroup>
                 </Card.Body>
             </Card>
-            <ListGroup className='d-flex w-100'>
-                {posts.map((post) => (
-                    <Card key={post.id} className='rounded-4 bg-white m-2'>
-                        <Card.Body className='p-10'>
-                            <div className="d-flex align-items-center">
-                               <img src="imgs/woman.png" alt="avatar" />
-                                <Card.Subtitle className="m-2 text-muted">{user.name}</Card.Subtitle>
-                            </div>
-                            <Card.Title>{post.title}</Card.Title>
-                            <Card.Text>{post.body}</Card.Text>
-                           <Comments postId={post.id} />
-                        </Card.Body>
-                    </Card>
-                ))}
-            </ListGroup>
+
         </Container>
     )
 }
