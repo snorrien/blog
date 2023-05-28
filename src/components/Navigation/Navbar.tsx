8bd21875fd4f2e0b54db47536e514d1c74b0d636
Alena Shapovalova <avshapovalova31@gmail.com>
2023-05-28 23:49:21 +0200

    bug: add page reload on links

--- src/components/Navigation/Navbar.tsx
+++ src/components/Navigation/Navbar.tsx
@@ -27,14 +27,14 @@ const Navbar = () => {
           <Container className="nav flex-column pt-2">
             <Nav className="nav nav-pills flex-column mt-4" >
               <Nav.Item className="nav-item custom-nav-item">
-                <NavLink className="nav-link d-flex align-items-center" to="/">
+                <NavLink className="nav-link d-flex align-items-center" to="/" reloadDocument>
                   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
                     <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
                   </svg>
                   <span className="fs-5 ms-3">Список постов</span></NavLink>
               </Nav.Item>
               <Nav.Item className="nav-item custom-nav-item">
-                <NavLink className="nav-link d-flex align-items-center" to="/about">
+                <NavLink className="nav-link d-flex align-items-center" to="/about" reloadDocument>
                   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-person" viewBox="0 0 16 16">
                     <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
                   </svg>
